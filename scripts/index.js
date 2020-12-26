d9ec8d29845276972be34a12777b2c9d79c5f2a6
Vladimir Chernyshov <tram27mrld@yandex.ru>
2020-12-26 17:59:50 +0300

    Исправления от 26.12.20

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,43 @@
-let editButton = document.querySelector('.profile__button_edit');
-let overlay = document.querySelector('.page__overlay');
-let closeButton = overlay.querySelector('.popup__button_close');
+let editButton = document.querySelector('.profile__button_edit'); 
+let overlay = document.querySelector('.page__overlay'); 
+let closeButton = overlay.querySelector('.popup__button_close'); 
+let nameInput = overlay.querySelector('.popup__item_el_name'); 
+let jobInput = overlay.querySelector('.popup__item_el_about'); 
+let name = document.querySelector('.profile__name'); 
+let job = document.querySelector('.profile__about'); 
+let form = overlay.querySelector('.popup'); 
+let likeButtons = document.querySelectorAll('.elements__button-like');
 
-let togglePopup = () => {
-  overlay.classList.toggle('page__overlay_active');
-};
+let openPopup = () => { 
+  overlay.classList.add('page__overlay_active'); 
+  handleFormSubmit();
+}; 
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-overlay.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget) {
-    togglePopup();
-  }
+let closePopup = () => { 
+  overlay.classList.remove('page__overlay_active'); 
+}; 
+
+function handleFormSubmit (evt) { 
+  evt.preventDefault();  
+  name.textContent = nameInput.value; 
+  job.textContent = jobInput.value; 
+  nameInput.setAttribute('value',name.textContent);  
+  jobInput.setAttribute('value',job.textContent); 
+  closePopup(); 
+} 
+
+overlay.addEventListener('click', (event) => { 
+  if (event.target === event.currentTarget) { 
+    closePopup(); 
+  } 
+}); 
+
+likeButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('elements__button-like_active');
+  });
 });
 
-let form = overlay.querySelector('.popup');
-
-function handleFormSubmit (evt) {
-  evt.preventDefault(); 
-  // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Находим поля формы в DOM
-  let nameInput = overlay.querySelector('.popup__item_el_name');
-  let jobInput = overlay.querySelector('.popup__item_el_about');
-
-  // Получите значение полей из свойства value
-  nameInput.getAttribute('value');
-  jobInput.getAttribute('value');
-
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let name = document.querySelector('.profile__name');
-  let job = document.querySelector('.profile__about');
-
-  // Вставьте новые значения с помощью textContent
-  name.textContent = nameInput.value;
-  job.textContent = jobInput.value;
-  togglePopup();
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-form.addEventListener('submit', handleFormSubmit); 
-
-let likeButton = document.querySelector('.elements__button-like');
-
-likeButton.addEventListener('click', () => {
-  likeButton.classList.toggle('elements__button-like_active');
-});
-
+editButton.addEventListener('click', openPopup); 
+closeButton.addEventListener('click', closePopup); 
+form.addEventListener('submit', handleFormSubmit);
